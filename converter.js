4cfed3f6067a3360ea2399951c6212e4910a7b15
UNEZALUCKY <143113106+UNEZALUCKY@users.noreply.github.com>
2024-11-14 21:12:16 +0530

    Update converter.js

--- converter.js
+++ converter.js
@@ -10,7 +10,7 @@ function convertCurrency() {
     }
 
     const exchangeRates = {
-        "RWF_INR": 0.0592, // 1 RWF = 0.0588 INR
+        "RWF_INR": 0.0587, // 1 RWF = 0.0588 INR
         "INR_RWF": 14.782,   // 1 INR = 14.94 RWF
     };
 
